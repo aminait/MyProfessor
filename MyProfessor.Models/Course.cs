6783fed05478391a5302df998db0833e8edba4d6
Amina Ait <45981655+aminait@users.noreply.github.com>
2020-07-27 12:32:17 +0300

    Add files via upload

--- MyProfessor.Models/Course.cs
+++ MyProfessor.Models/Course.cs
@@ -11,6 +11,7 @@ namespace MyProfessor.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Category { get; set; }
 
         public virtual ICollection<UserCourses> Users { get; set; }
         public virtual ICollection<CourseReview> Reviews { get; set; }
